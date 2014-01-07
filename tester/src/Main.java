22e5d7130109104cecfb9348f256a31f3e011904
Muffalo <e.strandberg90@gmail.com>
2014-01-07 21:57:45 +0100

    Update Main.java

--- tester/src/Main.java
+++ tester/src/Main.java
@@ -41,6 +41,7 @@ public class Main {
 		wo.addGameObject(new GameObject(100,100,50,50));
 		wo.addGameObject(new GameObject(150,100,50,50));
 		wo.addGameObject(new GameObject(200,200,100,300));
+		System.out.print("hej");
 	}
 
 }
